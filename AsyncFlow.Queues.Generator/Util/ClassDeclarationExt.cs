48958443ebc65c39c098ab689462272bc609cfd1
TheFo2sh <ahmed.fouad.net@hotmail.com>
2023-10-10 17:53:18 +0300

    7 enhancement request default hangfire queue name as class name for unattributed flows (#11)
    * Add support for class name as queue name
    
    * fixing job name
    
    * adding support for custom queue name using the QueueName attribute
    
    * ensure no duplicates in queue names
    
    * update readme.md
    
    * fix package version
    
    * change pipelinne to run on windows
    
    * use hangfire sqllite storage
    
    * modify the queuing mechanisim to use attribute instead of parameter

---
+++ AsyncFlow.Queues.Generator/Util/ClassDeclarationExt.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AsyncFlow.Queues.Generator.Util;
+
+public static class ClassDeclarationExt
+{
+    public static string? GetNamespaceFromClass(this ClassDeclarationSyntax classSyntax)
+    {
+        // For traditional namespaces
+        var traditionalNamespace = classSyntax.Ancestors().OfType<NamespaceDeclarationSyntax>().FirstOrDefault();
+        if (traditionalNamespace != null)
+        {
+            return traditionalNamespace.Name.ToString();
+        }
+        var parentCompilationUnit = classSyntax.SyntaxTree.GetCompilationUnitRoot();
+        return parentCompilationUnit
+            .ChildNodes()
+            .OfType<BaseNamespaceDeclarationSyntax>()
+            .First()
+            .Name
+            .ToString();
+    }
+    public static string? GetAttributeValue(this ClassDeclarationSyntax classDeclarationSyntax, string attributeName,
+        string propertyName)
+    {
+        return classDeclarationSyntax.AttributeLists
+            .SelectMany(attrList => attrList.Attributes)
+            .Where(attr => attr.Name.ToString() == attributeName)
+            .Select(attr =>
+                attr.ArgumentList?.Arguments.FirstOrDefault(arg =>
+                    arg.NameEquals?.Name.Identifier.Text == propertyName))
+            .Where(arg => arg != null)
+            .Select(arg => arg.Expression.ToString().Trim('"'))
+            .FirstOrDefault();
+    }
+
+    public static IEnumerable<string> GetBaseInterfaceGenericParameters(
+        this ClassDeclarationSyntax classDeclarationSyntax, string interfaceName)
+    {
+        
+        if (classDeclarationSyntax.BaseList?.Types.Count > 0)
+        {
+            foreach (var baseType in
+                     classDeclarationSyntax.BaseList.Types.Select(baseTypeSyntax => baseTypeSyntax.Type))
+            {
+                if (baseType is not GenericNameSyntax genericName || genericName.Identifier.Text != interfaceName)
+                    continue;
+
+                var typeArguments = genericName.TypeArgumentList.Arguments;
+
+                foreach (var typeArgument in typeArguments)
+                {
+                    yield return typeArgument.ToString();
+                }
+
+                break;
+            }
+        }
+        else
+            throw new Exception($"class does not implement interface {interfaceName}");
+    }
+}
